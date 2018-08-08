cbfce8cad8d55be6bc0e50d95d6e07eb790adb3a
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2018-08-08 10:49:30 +0200

    Convert precached to Rust 2018
    Convert tool: precached-trigger

--- src/bin/precached-trigger/src/util/config_file.rs
+++ src/bin/precached-trigger/src/util/config_file.rs
@@ -20,8 +20,9 @@
 
 use std::io;
 use std::path::{Path, PathBuf};
-use crate::util;
 use log::{trace, debug, info, warn, error, log, LevelFilter};
+use serde_derive::{Serialize, Deserialize};
+use crate::util;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct ConfigFile {
