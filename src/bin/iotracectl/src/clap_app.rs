c9ccb8ed950ebed918493e688ed7cdc482f91f04
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2018-08-10 22:14:46 +0200

    Port to Fluent Version 0.3
    Fix i18n, port to Fluent version 0.3

--- src/bin/iotracectl/src/clap_app.rs
+++ src/bin/iotracectl/src/clap_app.rs
@@ -60,7 +60,7 @@ pub fn get_app<'a, 'b>() -> App<'a, 'b> {
         .subcommand(
             SubCommand::with_name("status")
                 .setting(AppSettings::DeriveDisplayOrder)
-                .about(tr!("iotracectl-status"))
+                .about(tr!("iotracectl-show-status"))
                 .arg(Arg::with_name("tabular")
                             .long("tabular")
                             // .short("t")
